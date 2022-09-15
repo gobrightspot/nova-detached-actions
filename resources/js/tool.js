b7c153b9f24bdde6e6c9b3e352acdff188cdd86d
Adi Mihaila <adi.mihaila@outlook.com>
2022-09-15 10:33:05 +0300

    Rename ResourceDetail into ResourceDetailView

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -4,6 +4,6 @@ Nova.booting((app, store) => {
   app.component("ActionButton", require("./components/ActionButton.vue").default);
   app.component("ActionButtonGroup", require("./components/ActionButtonGroup.vue").default);
 
-  app.component("ResourceDetail", require("./nova-components/ResourceDetail.vue").default);
+  app.component("ResourceDetailView", require("./nova-components/ResourceDetailView.vue").default);
   app.component("ResourceTableToolbar", require("./nova-components/ResourceTableToolbar.vue").default);
 });
